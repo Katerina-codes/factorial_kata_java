31f17c5dc02c1b42c2c73bc599a7e8cec26229d3
Katerina <katerinaldg@gmail.com>
2018-02-27 12:42:16 +0000

    Returns factorial of 2

--- src/test/factorial/FactorialTest.java
+++ src/test/factorial/FactorialTest.java
@@ -16,7 +16,17 @@ public class FactorialTest {
         assertEquals(1, factorial(1));
     }
 
+    @Test
+    public void Two() {
+        assertEquals(2, factorial(2));
+    }
+
     private int factorial(int i) {
-        return 1;
+        if (i < 2) {
+            return 1;
+        } else {
+            return i;
+        }
+
     }
 }
